97d84ef73000e2264e576ea83e23e9d48466ab6d
Komlev Ivan <komleviv@gmail.com>
2023-03-31 11:05:41 +0300

    Task 1 completed

--- index.php
+++ index.php
@@ -17,14 +17,61 @@
 	<body>
 		<div class="container">
 			<div>
+				<b>Задание 1.<b><br>
+				<p>Сгенерировать 100 текстовых файлов с заданной структурой, каждый из которых содержит 100 000 строк.</p>
+			</div>
+			<div class="task">
 				<form action="index.php">
 					<button type="submit" class="button" name="action" value="generate">Сгенерировать файлы</button>
 				</form>
 			</div>
 			<div>
+				<b>Задание 2.<b><br>
+				<p>Реализовать объединение файлов в один. При объединении должна быть возможность удалить из всех файлов строки с заданным сочетанием символов, например, «abc» с выводом информации о количестве удаленных строк</p>
+			</div>
+			<div class="task">
 				<form action="index.php">
-					<input type="text" name="text" />
+					<label for="text">Cимволы для удаления: </label>
+					<input type="text" name="text" id="text" />
 					<button type="submit" class="button" name="action" value="unification">Объединить файлы</button>
+				</form>
+			</div>
+			<div>
+				<b>Задание 3.<b><br>
+				<p>Создать процедуру импорта файлов с таким набором полей в таблицу в СУБД. При импорте должен выводится ход процесса (сколько строк импортировано, сколько осталось)</p>
+			</div>
+			<div class="task">
+				<form method="post" action="src/classes.php" enctype = 'multipart/form-data'>
+					<input type="file" name="uploadedFile" />
+					<button type="submit" class="button" name="action" value="importtodb">Импортировать файл</button>
+				</form>
+			</div>
+			<div>
+				<b>Задание 4.<b><br>
+				<p>Реализовать хранимую процедуру в БД (или скрипт с внешним sql-запросом), который считает сумму всех целых чисел и медиану всех дробных чисел</p>
+			</div>
+			<div class="task">
+				<form method="post" action="src/classes.php">
+					<label for="file_name">Выбере файл:</label>
+					<select name="file_name" id="file_name">
+						<?php
+						$link = mysqli_connect("localhost", "root", "root", "b1");
+						$sql = 'SELECT file_name FROM files GROUP BY file_name';
+						$result = mysqli_query($link, $sql);
+
+						if ($result == false) {
+							print("Произошла ошибка при выполнении запроса");
+						}
+						
+						foreach ($result as $r) {
+							echo '<option value="'. $r["file_name"] .'">'. $r["file_name"] .'</option>';
+						}
+						
+						mysqli_close($link);
+					  ?>
+				  </select>
+					<button type="submit" class="button" name="action" value="totalint">Посчитать сумму челых чисел</button>
+					<button type="submit" class="button" name="action" value="medianfloat">Посчитать медиану дробных чисел</button>
 				</form>
 			</div>
 		</div>
--- src/classes.php
+++ src/classes.php
@@ -66,31 +66,151 @@ class Unification {
 	function unificationFiles($text) {
 		$save = fopen('../files/unification.txt', 'w');
 		
+		// Задаём директорию с файлами
 		$dir = opendir('../files');
 		$count = 0;
+		
+		// Просматриваем все файлы в директории
 		while (false !== ($file = readdir($dir))) {
+			
+			// Обрабатываем только файлы с названием 'file'
 			if (strpos($file, 'file') !== false) {
-				$count++;
+				
 				$f = fopen('../files/'.$file, 'r');
 				
+				// Задаём шаблон для поиска удаляемых символов без учёта регистра
+				$pattern = "/" . $text . "/i";
 				
-				
-				stream_copy_to_stream($f, $save);
+				// Построчно читаем текущий файл
+				while (($buffer = fgets($f)) !== false) {
+					
+						// Если в строке находим символы, соответствующие шаблону, очищаем строку и увеличиваем счётчик
+						if(preg_match($pattern, $buffer, $matches)) {
+							$buffer = '';		
+							$count++;
+						} 
+						
+						// Записываем оставшиеся строки в объединённый файл
+						fwrite($save, $buffer);
+										
+				}
+								
 				fclose($f);
 			}
 		}
-
 		fclose($save);
+		
+		return $count;
+	}
+}
+
+class DataBase {
+	function DataBaseWrite($filename, $tempname) {
+		$link = mysqli_connect("localhost", "root", "root", "b1");
+		
+		$count = 0;
+		$linesCount = $this->linesCount($tempname);
+		$f = fopen($tempname, 'r');
+		while (($buffer = fgets($f)) !== false) {
+			
+			$myArray = explode('||', $buffer ?? '');			
+				
+			$sql = 'INSERT INTO files (file_name, date, lat_symbol, rus_symbol, int_number, float_number) VALUES ("'. $filename .'", "'. date('Y-m-d', strtotime(str_replace('.', '/', $myArray[0]))) . '", "' . $myArray[1] .'", "'. iconv("cp1251", "utf-8", $myArray[2]) .'", "'. $myArray[3] .'", "' . $myArray[4] .'")';
+			
+			$result = mysqli_query($link, $sql);
+			
+			$count++;
+			
+			echo "В базу данных записано " . $count . " строк из " . $linesCount . "<br>";
+		}
+		
+		mysqli_close($link);
 	}
 	
-	function checkString($line, $text) {
-		if (strpos($line, $text) !== false) {
-			return false;
-		}
-		  return true;
-		}
-	}
+	function linesCount($file) 
+	{ 
+		// в начале ищем сам файл. Может быть, путь к нему был некорректно указан 
+		if(!file_exists($file))exit("Файл не найден"); 
+		 
+		// рассмотрим файл как массив
+		$file_arr = file($file); 
+		 
+		// подсчитываем количество строк в массиве 
+		$lines = count($file_arr); 
+		 
+		// вывод результата работы функции 
+		return $lines-1; 
+	} 
+	
+	function createTotalProcedure() {
+		$link = mysqli_connect("localhost", "root", "root", "b1");
+		
+		$sql = 'DROP PROCEDURE IF EXISTS "TOTAL";
 
+				DELIMITER //
+
+				CREATE PROCEDURE TOTAL(IN f_name VARCHAR(128))
+				BEGIN
+				SELECT SUM(int_number) FROM b1.files
+				WHERE file_name = f_name;
+				END //
+
+				DELIMITER ;';
+		
+		$result = mysqli_query($link, $sql);
+		
+		mysqli_close($link);
+	}
+	
+	function createMedianProcedure() {
+		$link = mysqli_connect("localhost", "root", "root", "b1");
+		
+		$sql = 'DROP PROCEDURE IF EXISTS "MEDIAN";
+
+				DELIMITER //
+
+				CREATE PROCEDURE MEDIAN()
+				BEGIN
+				SELECT AVG(ff.float_number) as median_val
+				FROM (
+				SELECT float_number, @rownum:=@rownum+1 as `row_number`, @total_rows:=@rownum
+				  FROM files f, (SELECT @rownum:=0) r
+				  WHERE f.float_number is NOT NULL AND f.file_name = f_name
+				  ORDER BY f.float_number
+				) as ff
+				WHERE ff.row_number IN ( FLOOR((@total_rows+1)/2), FLOOR((@total_rows+2)/2) );
+				END //
+
+				DELIMITER ;';
+		
+		$result = mysqli_query($link, $sql);
+		
+		mysqli_close($link);
+	}
+	
+	function totalInt($file_name) {
+		$link = mysqli_connect("localhost", "root", "root", "b1");
+		
+		$sql = 'CALL TOTAL("'.$file_name.'")';
+			
+		$result = mysqli_query($link, $sql);
+		
+		mysqli_close($link);
+			
+	}
+	
+	function medianFloat($file_name) {
+		$link = mysqli_connect("localhost", "root", "root", "b1");
+		
+		$sql = 'CALL MEDIAN("'.$file_name.'")';
+			
+		$result = mysqli_query($link, $sql);
+		
+		mysqli_close($link);
+	}
+}
+
+// В зависимости от атрибута в адресе запускаем необходимую функцию
 if (isset($_POST['action'])) {
 	switch ($_POST['action']) {
 		case 'generate':
@@ -101,13 +221,24 @@ if (isset($_POST['action'])) {
 			if (isset($_POST['text'])) {
 				$un = new Unification();
 				$un->unificationFiles($_POST['text']);
+				return;
 			}
+			break;
+		case 'importtodb':
+			$filename = basename($_FILES['uploadedFile']['name']);
+			$tempname = $_FILES['uploadedFile']['tmp_name'];
+			
+			$db = new DataBase();
+			$db->DataBaseWrite($filename, $tempname);
+			break;
+		case 'totalint':
+			$db = new DataBase();
+			$db->totalInt($_POST['file_name']);
+			break;
+		case 'medianfloat':
+			$db = new DataBase();
+			$db->medianFloat($_POST['file_name']);
 			break;
 	}
 }
-
-//$un = new Unification();
-//$un->unificationFiles();
-$gn = new Generation();
-$gn->filesGeneration()
 ?>
